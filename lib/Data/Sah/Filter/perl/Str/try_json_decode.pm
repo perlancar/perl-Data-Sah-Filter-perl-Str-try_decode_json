e161d98f7ff76bf7b6c5a1f8375582276c8d0541
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-02-11 21:34:30 +0700

    Initial commit

---
+++ lib/Data/Sah/Filter/perl/Str/try_json_decode.pm
@@ -0,0 +1,46 @@
+package Data::Sah::Filter::perl::Str::try_json_decode;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+sub meta {
+    +{
+        v => 1,
+        summary => 'JSON-decode if we can, otherwise leave string as-is',
+    };
+}
+
+sub filter {
+    my %args = @_;
+
+    my $dt = $args{data_term};
+
+    my $res = {};
+
+    $res->{modules}{"JSON::PP"} //= 0;
+    $res->{expr_filter} = join(
+        "",
+        "do { my \$decoded; eval { \$decoded = JSON::PP->new->allow_nonref->decode($dt); 1 }; \$@ ? $dt : \$decoded }",
+    );
+
+    $res;
+}
+
+1;
+# ABSTRACT:
+
+=for Pod::Coverage ^(meta|filter)$
+
+=head1 DESCRIPTION
+
+This rule is sometimes convenient if you want to accept unquoted string or a
+data structure (encoded in JSON). This means, compared to just decoding from
+JSON, you don't have to always quote your string. But beware of traps like the
+bare values C<null>, C<true>, C<false> becoming undef/1/0 in Perl instead of
+string literals, because they can be JSON-decoded.
